4f6191c07b34d15aade531cc88c2ca4df34008fd
Pritha Bisarad <51969517+bisaradpritha@users.noreply.github.com>
2026-01-28 09:55:21 -0700

    Update MAPCP_Figure4A_4B.R

--- MAPCP_Figure4/MAPCP_Figure4A_4B.R
+++ MAPCP_Figure4/MAPCP_Figure4A_4B.R
@@ -6,7 +6,7 @@ library(ggpubr)
 library(readxl)
 
 ## Figure 4A ##
-rohData <- read_excel("/path/to/directory/SupplementaryTables_Revised.xlsx", sheet = "Supplementary Table 19", skip=1, col_names = TRUE) #change /path/to/directory to where the supplementary tables are
+rohData <- read_excel("/path/to/directory/SupplementaryTables_11.05.25.xlsx", sheet = "Supplementary Table 18", skip=1, col_names = TRUE) #change /path/to/directory to where the supplementary tables are
 
 # Calculating quartile values for each cohort
 gccpQuartile <- quantile(rohData$Tot_roh_proband_Mb[rohData$Cohort=='internal_control_cohort'], probs = c(0,0.25,0.5,0.75,1))
@@ -33,23 +33,23 @@ ggsave("Total_ROH_Size_vs_Reported_Consanguinity.pdf", plot = plt1)
 
 
 ## Figure 4B ##
-ConsangunityDf = read_excel("/path/to/directory/SupplementaryTables_Revised.xlsx", sheet = "Supplementary Table 20", skip=1, col_names = TRUE) #change /path/to/directory to where the supplementary tables are
+ConsangunityDf = read_excel("/path/to/directory/SupplementaryTables_11.05.25.xlsx", sheet = "Supplementary Table 19", skip=1, col_names = TRUE) #change /path/to/directory to where the supplementary tables are
 
-plt1 = ggplot(ConsangunityDf, aes(x = Expected_Percentage_of_ROH_burden, y = Observed_Percentage_of_ROH_burden))
-plt1 = plt1+geom_point()+geom_smooth(aes(Expected_Percentage_of_ROH_burden,Observed_Percentage_of_ROH_burden), method="lm")
-plt1 = plt1+theme_classic()
+plt1 <- ggplot(ConsangunityDf, aes(x = Expected_Percentage_of_ROH_burden, y = Observed_Percentage_of_ROH_burden))
+plt1 <- plt1+geom_point()+geom_smooth(aes(Expected_Percentage_of_ROH_burden,Observed_Percentage_of_ROH_burden), method="lm")
+plt1 <- plt1+theme_classic()
 plt1
 
 # Performing paired t-test
-t_test_result = t.test(ConsangunityDf$Observed_Percentage_of_ROH_burden, ConsangunityDf$Expected_Percentage_of_ROH_burden, paired = TRUE)
+t_test_result <- t.test(ConsangunityDf$Observed_Percentage_of_ROH_burden, ConsangunityDf$Expected_Percentage_of_ROH_burden, paired = TRUE)
 print(t_test_result)
 
-pval = signif(t_test_result$p.value, 4)
-tval = signif(t_test_result$statistic, 4)
-degfree = t_test_result$parameter
+pval <- signif(t_test_result$p.value, 4)
+tval <- signif(t_test_result$statistic, 4)
+degfree <- t_test_result$parameter
 
-plt1 = plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed_Percentage_of_ROH_burden), label = paste0("t-test p value = ",pval), size = 5)
-plt1 = plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed_Percentage_of_ROH_burden)+1, label = paste0("t-test t value (",degfree,") = ",tval), size = 5)
+plt1 <- plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed_Percentage_of_ROH_burden), label = paste0("t-test p value = ",pval), size = 5)
+plt1 <- plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed_Percentage_of_ROH_burden)+1, label = paste0("t-test t value (",degfree,") = ",tval), size = 5)
 plt1
 ggsave("Total_ROH_Size_Observed_vs_Expected.pdf", plot = plt1)
 ###############
@@ -58,21 +58,22 @@ ggsave("Total_ROH_Size_Observed_vs_Expected.pdf", plot = plt1)
 ## Figure 4B ##
 ConsangunityDf = read_csv("Data/mapcp/Tables/MAPCP_ROH_KnownPedigreeFams_Obs_vs_Exp_ROC_08202025.csv")
 
-plt1 = ggplot(ConsangunityDf, aes(x = Expected, y = Observed))
-plt1 = plt1+geom_point()+geom_smooth(aes(Expected,Observed), method="lm")
-plt1 = plt1+theme_classic()
+plt1 <- ggplot(ConsangunityDf, aes(x = Expected, y = Observed))
+plt1 <- plt1+geom_point()+geom_smooth(aes(Expected,Observed), method="lm")
+plt1 <- plt1+theme_classic()
 plt1
 
 # Performing paired t-test
 t_test_result = t.test(ConsangunityDf$Observed, ConsangunityDf$Expected, paired = TRUE)
 print(t_test_result)
 
-pval = signif(t_test_result$p.value, 4)
-tval = signif(t_test_result$statistic, 4)
-degfree = t_test_result$parameter
+pval <- signif(t_test_result$p.value, 4)
+tval <- signif(t_test_result$statistic, 4)
+degfree <- t_test_result$parameter
 
-plt1 = plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed), label = paste0("t-test p value = ",pval), size = 5)
-plt1 = plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed)+1, label = paste0("t-test t value (",degfree,") = ",tval), size = 5)
+plt1 <- plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed), label = paste0("t-test p value = ",pval), size = 5)
+plt1 <- plt1+annotate("text", x = 1.5, y = max(ConsangunityDf$Observed)+1, label = paste0("t-test t value (",degfree,") = ",tval), size = 5)
 ggsave("Total_ROH_Size_Observed_vs_Expected.pdf", plot = plt1)
 
 ###############
+
